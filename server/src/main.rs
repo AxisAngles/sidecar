5e14f1618945eaf36981197a8640b1a4a00feeb9
Trey Reynolds <axisangles@gmail.com>
2025-10-21 05:59:09 -0700

    implement server with write_file endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+enum Error {
+	NoNewlineToSeparatePath,
+	InvalidPath,
+	IO(std::io::Error),
+}
+
+impl axum::response::IntoResponse for Error {
+	fn into_response(self) -> axum::response::Response {
+		use axum::http::StatusCode;
+		use axum::response::Response;
+		match self{
+			Error::NoNewlineToSeparatePath=>(StatusCode::INTERNAL_SERVER_ERROR, "NoNewlineToSeparatePath").into_response(),
+			Error::InvalidPath=>Response::builder().status(500).body("InvalidPath".into()).unwrap(),
+			Error::IO(io_error)=>Response::builder().status(500).body(io_error.to_string().into()).unwrap(),
+		}
+	}
+}
+
+// path = game/whatever/code.luau
+async fn write_file(body: axum::body::Bytes) -> Result<(), Error> {
+	let body = body.as_ref();
+	let path_position = body
+		.iter()
+		.position(|c|*c==b'\n')
+		.ok_or(Error::NoNewlineToSeparatePath)?;
+
+	let path = &body[0 .. path_position];
+	let code = &body[path_position + 1 ..];
+
+	let path_str=str::from_utf8(path).map_err(|_|Error::InvalidPath)?;
+
+	let mut path = std::path::PathBuf::from(path_str);
+	let file_name=path.file_name().ok_or(Error::InvalidPath)?.to_owned();
+
+	path.pop();
+
+	// guaranteeFolderPath(path)
+	tokio::fs::create_dir_all(path).await.map_err(Error::IO)?;
+	// create the file
+	tokio::fs::write(file_name, code).await.map_err(Error::IO)?;
+
+	Ok(())
+}
+
+
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error>{
+	use axum::routing::{get, post};
+	println!("Starting Sidecar");
+
+	let addr=std::net::SocketAddr::from(([127,0,0,1], 8080));
+	let listener=tokio::net::TcpListener::bind(addr).await?;
+
+	let app=axum::Router::new()
+		.route("/write_file", post(write_file))
+		.route("/write_file", get("heyo"));
+
+	axum::serve(listener, app).await?;
+
+	Ok(())
 }
